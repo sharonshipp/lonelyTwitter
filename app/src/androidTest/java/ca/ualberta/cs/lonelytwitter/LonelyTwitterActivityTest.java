926b1d84782b7493cc093a39b4fdf9efd4b40e17
sharonshipp <qi5@ualberta.ca>
2018-10-25 21:42:31 -0600

    Add files via upload

--- app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
+++ app/src/androidTest/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivityTest.java
@@ -1,21 +1,41 @@
 package ca.ualberta.cs.lonelytwitter;
 
-import android.app.Activity;
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.EditText;
 
-import junit.framework.TestCase;
+import com.robotium.solo.Solo;
 
-/**
- * Created by wz on 14/09/15.
- */
-public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2 {
+public class LonelyTwitterActivityTest extends ActivityInstrumentationTestCase2<LonelyTwitterActivity> {
+    private Solo solo;
 
-    public LonelyTwitterActivityTest() {
-        super(ca.ualberta.cs.lonelytwitter.LonelyTwitterActivity.class);
-    }
-
-    public void testStart() throws Exception {
-        Activity activity = getActivity();
+    public LonelyTwitterActivityTest(){
+        super(LonelyTwitterActivity.class);
 
     }
+
+    @Override
+    public void setUp(){
+        solo = new Solo(getInstrumentation(), getActivity());
+    }
+
+    @Override
+    public void tearDown(){
+        solo.finishOpenedActivities();
+    }
+
+    public void testTweet(){
+        solo.assertCurrentActivity("wrong activity","LonelyTwitterActivity");
+    }
+
+    public void testEqual(){
+        assertEquals("not equal","5","6");
+    }
+
+    public void testAddTweet(){
+        solo.enterText((EditText) solo.getView(R.id.body),"new thing");
+        solo.clickOnButton("Save");
+        solo.clearEditText((EditText) solo.getView(R.id.body));
+        assertTrue(solo.waitForText("new thing"));
+    }
+
 }

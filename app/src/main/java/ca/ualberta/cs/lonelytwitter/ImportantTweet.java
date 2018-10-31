2010216792e123bf229f3ad57e550414335c0c9c
sharonshipp <sharonshipp@sharonshippdeMacBook-Air.local>
2018-10-30 20:44:47 -0600

    lab

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/ImportantTweet.java
@@ -2,7 +2,7 @@ package ca.ualberta.cs.lonelytwitter;
 
 public class ImportantTweet extends Tweet {
 
-    public ImportantTweet(String message){
+    public ImportantTweet(String message) throws TweetTooLongException{
         super(message);
     }
 
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -2,18 +2,19 @@ package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
-public abstract class Tweet {
+public abstract class SimpleTweet {
+    public static final int MAX_CHARS = 140;
     public String message;
-    public Date date;
+    private Date date;
 
-    public Tweet(String message){
-        this.message = message;
-        this.date = new Date();
+    public SimpleTweet(String message) throws TweetTooLongException{
+        this.setMessage(message);
+        this.setDate(new Date());
     }
 
-    public Tweet(String message, Date date){
-        this.message = message;
-        this.date = date;
+    public SimpleTweet(String message, Date date) throws TweetTooLongException{
+        this.setMessage(message);
+        this.setDate(date);
     }
 
     @Override
@@ -24,7 +25,7 @@ public abstract class Tweet {
     public abstract Boolean isImportant();
 
     public void setMessage(String message) throws TweetTooLongException {
-        if (message.length() > 140){
+        if (message.length() > MAX_CHARS){
             //Do Something!
             throw new TweetTooLongException();
         }
@@ -36,4 +37,11 @@ public abstract class Tweet {
     }
 
 
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
